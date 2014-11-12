d48a5848e5c48082647b8a2c20e7887cbca8c24c
Julio Reyes <julio@elumen.info>
2014-11-12 19:22:10 -0300

    static files

--- mecmapi/views.py
+++ mecmapi/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.views.generic import View
 from django.http import HttpResponse
 
 class Index(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('django 1.7 on Openshift')
+        return render_to_response('test.html', context_instance=RequestContext(request))

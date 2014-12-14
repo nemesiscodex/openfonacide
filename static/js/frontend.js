cb7d149e43889a4574557415e9c8b66033a18e62
Julio Reyes <nemesiscodex@gmail.com>
2014-12-14 16:12:17 -0300

    mockup de prioridades

--- static/js/frontend.js
+++ static/js/frontend.js
@@ -256,16 +256,16 @@
 
             $scope.mapData = data;
             sidebar.addTo($scope.map);
-            $( "#slider-range-min" ).slider({
-      range: "min",
-      value: 37,
-      min: 1,
-      max: 700,
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "" + ui.value );
-      }
-    });
-            $( "#amount" ).val( "" + $( "#slider-range-min" ).slider( "value" ) );
+//            $( "#slider-range-min" ).slider({
+//      range: "min",
+//      value: 37,
+//      min: 1,
+//      max: 700,
+//      slide: function( event, ui ) {
+//        $( "#amount" ).val( "" + ui.value );
+//      }
+//    });
+//            $( "#amount" ).val( "" + $( "#slider-range-min" ).slider( "value" ) );
 
 
             $scope.update('');

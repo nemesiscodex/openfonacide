d3698f7cc2a730d30a6aa83f6b7259fb919a331d
Diego Ramírez <syncrhonous@gmail.com>
2015-04-05 09:13:13 -0400

    Test para establecimmiento
    Signed-off-by: Diego Ramírez <syncrhonous@gmail.com>

--- openfonacide/tests.py
+++ openfonacide/tests.py
@@ -1,17 +1,35 @@
 # coding=utf-8
 from django.test import TestCase
+
 from openfonacide.models import Establecimiento
 from openfonacide.utils import conversion
 
 
 class EstablecimientoTest(TestCase):
     def setUp(self):
-        i = Establecimiento.objects.create(nombre="Institucion1", anio="2015")
+        i = Establecimiento.objects.create(
+            nombre="Institucion1",
+            anio="2015",
+            codigo_establecimiento="00112233"
+        )
         i.save()
 
     def test_db_access(self):
         k = Establecimiento.objects.get(anio="2015")
         self.assertEqual(k.nombre, "Institucion1")
+        self.assertEqual("00112233", k.codigo_establecimiento)
+        self.assertEqual(None, k.fonacide)
+
+    def test_unique_fields(self):
+        try:
+            nuevo_objeto = Establecimiento.objects.create(
+                nombre="DifferentNameBut",
+                anio="2015",
+                codigo_establecimiento="00112233"
+            )
+            nuevo_objeto.save()
+        except Exception, e:
+            self.assertRegexpMatches(e.message, "columns anio, codigo_establecimiento are not unique")
 
 
 class UtilsTest(TestCase):
@@ -28,29 +46,29 @@ class UtilsTest(TestCase):
         x = u'20° E'
         ny = conversion(y)
         nx = conversion(x)
-        self.assertEqual("%0.6f"%ny,'30.000000')
-        self.assertEqual("%0.6f"%nx,'20.000000')
+        self.assertEqual("%0.6f" % ny, '30.000000')
+        self.assertEqual("%0.6f" % nx, '20.000000')
 
     def test_conversion_south_west(self):
         y = u'30° S'
         x = u'10° 30\' W'
         ny = conversion(y)
         nx = conversion(x)
-        self.assertEqual("%0.6f"%ny,'-30.000000')
-        self.assertEqual("%0.6f"%nx,'-10.500000')
+        self.assertEqual("%0.6f" % ny, '-30.000000')
+        self.assertEqual("%0.6f" % nx, '-10.500000')
 
     def test_conversion_south_east(self):
         y = u'100° 30\' S'
         x = u'10° 70\' E'
         ny = conversion(y)
         nx = conversion(x)
-        self.assertEqual("%0.6f"%ny,'-100.500000')
-        self.assertEqual("%0.6f"%nx,'11.166667')
+        self.assertEqual("%0.6f" % ny, '-100.500000')
+        self.assertEqual("%0.6f" % nx, '11.166667')
 
     def test_conversion_cero(self):
         y = u'0° N'
         x = u'0° N'
         ny = conversion(y)
         nx = conversion(x)
-        self.assertEqual("%0.6f"%ny, '0.000000')
-        self.assertEqual("%0.6f"%nx, '0.000000')
+        self.assertEqual("%0.6f" % ny, '0.000000')
+        self.assertEqual("%0.6f" % nx, '0.000000')

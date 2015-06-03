561920908684e51cf47928e7e825369f1e0043ba
Julio Reyes <nemesiscodex@gmail.com>
2015-06-03 01:32:50 -0400

    Mejora el resumen visualmente
    - Igual al filtro del mapa
    - Agrega un loader
    - Agrega un dimmer al inicio

--- static/js/frontend.ResumenController.js
+++ static/js/frontend.ResumenController.js
@@ -266,6 +266,8 @@
 
     angular.module('frontEnd')
       .controller('ResumenController', ['$scope', '$timeout', 'backEnd', function($scope, $timeout, backEnd){
+        $scope.loading = false;
+        $scope.sin_datos = true;
         require.config({
             paths: {
                 echarts: 'static/echarts'
@@ -302,147 +304,10 @@
         };
         cargarAnio();
 
-        var cargarUbicacion = function(){
-          //PLACEHOLDER
-          backEnd.ubicaciones.get({}, function(data){
-            var ubicaciones = data;
-
-            $scope.distritos = [];
-            $scope.barrios = [];
-            $scope.ubicaciones = ubicaciones;
-            $scope.$watch('departamentoSelected', function(){
-            $scope.distritos = [];
-            $scope.distritoSelected = "";
-            $scope.barrioSelected = "";
-            for(i in $scope.ubicaciones){
-              if($scope.ubicaciones[i].id == $scope.departamentoSelected){
-                $scope.distritos = $scope.ubicaciones[i].distritos;
-                break;
-              }
-            }
-            //Esto es por https://docs.angularjs.org/error/$rootScope/inprog?p0=$digest#triggering-events-programmatically
-            $timeout(function(){
-              $('#ubicacion-distrito .dropdown,#ubicacion-barrio .dropdown').dropdown('clear');
-            },0,false);
-
-            });
-            $scope.$watch('distritoSelected', function(){
-            $scope.barrios = [];
-            $scope.barrioSelected = "";
-            for(i in $scope.distritos){
-              if($scope.distritos[i].id == $scope.distritoSelected){
-                $scope.barrios = $scope.distritos[i].barrios;
-                break;
-              }
-            }
-            $timeout(function(){
-              $('#ubicacion-barrio .dropdown').dropdown('clear');
-            },0,false);
-            });
-
-            $timeout(function(){
-            // $('.ui.checkbox').checkbox();
-            $('.dropdown').dropdown();
-            },300, false);
-          });
-
-        };
-        cargarUbicacion();
-        $scope.agregarUbicacion = function(){
-          if(!$scope.departamentoSelected){
-            return;
-          }
-          var flecha = " &#x279c; ";
-          var seleccion = "";
-          if($scope.ubicacionesSeleccionadas
-            .filter(function(obj){
-              return obj[0] == $scope.departamentoSelected
-                &&  obj[1] == $scope.distritoSelected
-                &&  obj[2] == $scope.barrioSelected;
-            }).length > 0){
-              return;
-          }
-          $scope.ubicacionesSeleccionadas.push(
-            [$scope.departamentoSelected, $scope.distritoSelected, $scope.barrioSelected]
-          );
-          var depObj, disObj, barObj;
-
-          depObj = $scope.ubicaciones
-            .filter(function(obj){
-              return obj.id == $scope.departamentoSelected;
-              });
-          seleccion += depObj
-            .map(function(obj){
-              return obj.nombre;
-              })
-            .reduce(function(a,b){
-              return a+b;
-              });
-
-          depObj = depObj[0];
-          if($scope.distritoSelected){
-            disObj = depObj.distritos
-              .filter(function(obj){
-                return obj.id == $scope.distritoSelected;
-                });
-            seleccion += flecha + disObj
-              .map(function(obj){
-                return obj.nombre;
-                })
-              .reduce(function(a,b){
-                return a+b;
-                });
-            disObj = disObj[0];
-          }
-          if($scope.barrioSelected){
-            barObj = disObj.barrios
-              .filter(function(obj){
-                return obj.id == $scope.barrioSelected;
-                });
-            seleccion += flecha + barObj
-              .map(function(obj){
-                return obj.nombre;
-                })
-              .reduce(function(a,b){
-                return a+b;
-                });
-              barObj = barObj[0];
-          }
-          $('#ubicacion-labels').append(
-            $('<div class="ui label" style="margin-bottom: 5px" data-dep="'+$scope.departamentoSelected+'" data-dis="'+$scope.distritoSelected+'" data-bar="'+$scope.barrioSelected+'"></div>')
-              .html(seleccion)
-              .append(
-                $('<i class="delete icon"></i>')
-                .click(function(){
-                  var label = $(this).parent();
-                  $scope.ubicacionesSeleccionadas = $scope.ubicacionesSeleccionadas
-                    .filter(function(obj){
-                      return !(obj[0] == $scope.departamentoSelected
-                        &&  obj[1] == $scope.distritoSelected
-                        &&  obj[2] == $scope.barrioSelected);
-                    });
-                  label.remove();
-                  $scope.$digest();
-                })
-              )
-            );
-        };
-
-        $scope.limpiarUbicacion = function(){
-          $timeout(function(){
-            $('#ubicacion .ui.dropdown').dropdown('clear');
-          }, 0, false);
-        };
-
-        $scope.borrarUbicaciones = function(){
-          $scope.ubicacionesSeleccionadas = [];
-          $timeout(function(){
-            $('#ubicacion-labels .ui.label').remove();
-            $scope.$digest();
-          }, 0, false);
-        };
 
         $scope.actualizarResumen = function(){
+          $scope.loading = true;
+          $scope.sin_datos = false;
           var resumen = {};
           resumen.anios = $scope.anioSelected;
           if($scope.ubicacionCheck){
@@ -460,6 +325,9 @@
 
           console.log(resumen);
           backEnd.resumen.get({params: resumen}, function(data){
+
+              $scope.loading = false;
+
               $scope.data = data;
 
               var charts = [];
@@ -546,7 +414,7 @@
                         window.charts[index].resize();
                     }
                 }
-              })
+              });
           });
 
         };

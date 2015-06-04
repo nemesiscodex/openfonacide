459124baafcf3efd7263cdda2f0e98945920d8c5
Julio Reyes <nemesiscodex@gmail.com>
2015-06-04 19:14:05 -0400

    Agrega filtro de acuerdo al modelo creado

--- openfonacide/filtros.py
+++ openfonacide/filtros.py
@@ -39,7 +39,6 @@ def generar_ubicacion(request):
             bar = {'id': row[4], 'nombre': row[5]}
             dis.get('barrios').append(bar)
 
-
         anterior[0] = row[0]
         anterior[1] = row[2]
         anterior[2] = row[4]
@@ -55,7 +54,7 @@ def filtros(request):
     # adjudicaciones = request.GET.get('adjudicaciones')
     # planificaciones = request.GET.get('planificaciones')
     estado = request.GET.get('estado')
-    if documentos or estado and estado.lower() in ('priorizado', 'planificado', 'adjudicado', 'terminado'):
+    if reportadas or documentos or estado and estado.lower() in ('priorizado', 'planificado', 'adjudicado', 'terminado'):
         if not prioridades:
             prioridades = json.dumps({'mobiliarios': True, 'sanitarios': True, 'aulas': True, 'otros': True})
     if prioridades:
@@ -69,7 +68,7 @@ def filtros(request):
         for key in prioridades:
             if prioridades.get(key):
                 tipo.append(key)
-        return JsonResponse(filtro_prioridad(tipo, rango, ubicacion, estado, documentos), safe=False)
+        return JsonResponse(filtro_prioridad(tipo, rango, ubicacion, estado, documentos, reportadas), safe=False)
     else:
         if ubicacion:
             ubicacion = json.loads(ubicacion)
@@ -99,9 +98,9 @@ def get_rango(rango):
     return ret
 
 
-def filtro_prioridad(tipo, rango, ubicacion, estado, documentos):
+def filtro_prioridad(tipo, rango, ubicacion, estado, documentos, reportadas):
     cursor = connection.cursor()
-    cursor.execute(query_prioridad(tipo, rango, ubicacion, estado, documentos))
+    cursor.execute(query_prioridad(tipo, rango, ubicacion, estado, documentos, reportadas))
     rows = cursor.fetchall()
     rows = map(lambda x: x[0], rows)
     cursor.close()
@@ -110,13 +109,17 @@ def filtro_prioridad(tipo, rango, ubicacion, estado, documentos):
     return rows
 
 
-def query_prioridad(tipo, rango, ubicacion, estado, documentos):
+def query_prioridad(tipo, rango, ubicacion, estado, documentos, reportadas):
     begin_query = ('SELECT DISTINCT codigo_establecimiento FROM (')
     union = False
 
     query_estado = ''
     if estado and estado.lower() in ('priorizado', 'planificado', 'adjudicado', 'terminado'):
         query_estado = " AND lower(estado_de_obra) = '%s' " % estado
+
+    query_reportadas = ''
+    if reportadas:
+        query_reportadas = " JOIN openfonacide_reportes re ON re.id_prioridad = %s.id AND re.tipo = '%s' "
 
     query_documentos = ''
     if documentos:
@@ -142,8 +145,10 @@ def query_prioridad(tipo, rango, ubicacion, estado, documentos):
         begin_query += ('SELECT p_mob.codigo_establecimiento '
                 'FROM openfonacide_mobiliario p_mob '
                 'JOIN openfonacide_establecimiento es '
-                'ON es.codigo_establecimiento = p_mob.codigo_establecimiento '
-                'WHERE p_mob.numero_prioridad >= '+ str(rango[0]) +
+                'ON es.codigo_establecimiento = p_mob.codigo_establecimiento ')
+        if reportadas:
+            begin_query += query_reportadas % ('p_mob', 'mobiliarios')
+        begin_query += ('WHERE p_mob.numero_prioridad >= '+ str(rango[0]) +
                 ' AND p_mob.numero_prioridad <= ' + str(rango[1]))
         begin_query += query_documentos
         begin_query += query_estado
@@ -155,8 +160,10 @@ def query_prioridad(tipo, rango, ubicacion, estado, documentos):
         begin_query += ('SELECT p_san.codigo_establecimiento '
                 'FROM openfonacide_sanitario p_san '
                 'JOIN openfonacide_establecimiento es '
-                'ON es.codigo_establecimiento = p_san.codigo_establecimiento '
-                'WHERE p_san.numero_prioridad >= '+ str(rango[0]) +
+                'ON es.codigo_establecimiento = p_san.codigo_establecimiento ')
+        if reportadas:
+            begin_query += query_reportadas % ('p_san', 'sanitarios')
+        begin_query += ('WHERE p_san.numero_prioridad >= '+ str(rango[0]) +
                 ' AND p_san.numero_prioridad <= ' + str(rango[1]))
         begin_query += query_documentos
         begin_query += query_estado
@@ -168,8 +175,10 @@ def query_prioridad(tipo, rango, ubicacion, estado, documentos):
         begin_query += ('SELECT p_au.codigo_establecimiento '
                 'FROM openfonacide_espacio p_au '
                 'JOIN openfonacide_establecimiento es '
-                'ON es.codigo_establecimiento = p_au.codigo_establecimiento '
-                ' where p_au.nombre_espacio is null '
+                'ON es.codigo_establecimiento = p_au.codigo_establecimiento ')
+        if reportadas:
+            begin_query += query_reportadas % ('p_au', 'aulas')
+        begin_query += (' where p_au.nombre_espacio is null '
                 ' and p_au.numero_prioridad >= ' + str(rango[0]) +
                 ' and p_au.numero_prioridad <= ' + str(rango[1]))
         begin_query += query_documentos
@@ -182,8 +191,10 @@ def query_prioridad(tipo, rango, ubicacion, estado, documentos):
         begin_query += ('SELECT p_es.codigo_establecimiento '
                 'FROM openfonacide_espacio p_es '
                 'JOIN openfonacide_establecimiento es '
-                'ON es.codigo_establecimiento = p_es.codigo_establecimiento '
-                ' where p_es.nombre_espacio is not null '
+                'ON es.codigo_establecimiento = p_es.codigo_establecimiento ')
+        if reportadas:
+            begin_query += query_reportadas % ('p_es', 'otros')
+        begin_query += (' where p_es.nombre_espacio is not null '
                 ' and p_es.numero_prioridad >= ' + str(rango[0]) +
                 ' and p_es.numero_prioridad <= ' + str(rango[1]))
         begin_query += query_documentos
